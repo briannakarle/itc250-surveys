6d22183d2ab11b38219910e4eb7a535388c429e7
Brianna Karle <briannarisard@gmail.com>
2016-03-01 15:25:34 -0800

    Create working list page

--- surveys/index.php
+++ surveys/index.php
@@ -1,22 +1,46 @@
 <?php
 /**
- * index.php is the survey list page for the surveysez app
+ *index.php the list page for our surveys, based on demo_list_pager
  *
- * @package nmCommon
+ *demo_list_pager.php along with demo_view_pager.php provides a sample web application
+ *
+ * The difference between demo_list.php and demo_list_pager.php is the reference to the 
+ * Pager class which processes a mysqli SQL statement and spans records across multiple  
+ * pages. 
+ *
+ * The associated view page, demo_view_pager.php is virtually identical to demo_view.php. 
+ * The only difference is the pager version links to the list pager version to create a 
+ * separate application from the original list/view. 
+ * 
+ * @package nmPager
  * @author Bill Newman <williamnewman@gmail.com>
- * @version 2.09 2011/05/09
+ * @version 3.02 2011/05/18
  * @link http://www.newmanix.com/
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
- * @see config_inc.php  
- * @see header_inc.php
- * @see footer_inc.php 
+ * @see demo_view_pager.php
+ * @see Pager.php 
  * @todo none
  */
-# '../' works for a sub-folder.  use './' for the root
-require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials
 
-$config->titleTag = smartTitle(); #Fills <title> tag. If left empty will fallback to $config->titleTag in config_inc.php
-$config->metaDescription = smartTitle() . ' - ' . $config->metaDescription; 
+# '../' works for a sub-folder.  use './' for the root  
+require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
+ 
+# SQL statement
+$sql =  
+"
+select CONCAT(a.FirstName, ' ', a.LastName) AdminName, s.SurveyID, s.Title, s.Description, 
+date_format(s.DateAdded, '%W %D %M %Y %H:%i') 'DateAdded' from "
+. PREFIX . "surveys s, " . PREFIX . "Admin a where s.AdminID=a.AdminID order by s.DateAdded desc
+";
+
+
+#Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
+$config->titleTag = 'Surveys made with love & PHP in Seattle';
+
+#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
+$config->metaDescription = 'Seattle Central\'s ITC250 Class Surveys are made with pure PHP! ' . $config->metaDescription;
+$config->metaKeywords = 'Surveys,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
+
 /*
 $config->metaDescription = 'Web Database ITC281 class website.'; #Fills <meta> tags.
 $config->metaKeywords = 'SCCC,Seattle Central,ITC281,database,mysql,php';
@@ -30,31 +54,65 @@ $config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewabl
 $config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
 */
 
-# SQL statement - PREFIX is optional way to distinguish your app
-$sql = "select * from wn16_surveys";
+# END CONFIG AREA ---------------------------------------------------------- 
 
-//END CONFIG AREA ---------------------------------------------------------- 
-
-get_header(); #defaults to header_inc.php
+get_header(); #defaults to theme header or header_inc.php
 ?>
-<h3 align="center">Surveys</h3>
+<h3>Surveys</h3>
 
 <?php
-#IDB::conn() creates a shareable database connection via a singleton class
+#reference images for pager
+$prev = '<img src="' . VIRTUAL_PATH . 'images/arrow_prev.gif" border="0" />';
+$next = '<img src="' . VIRTUAL_PATH . 'images/arrow_next.gif" border="0" />';
+
+# Create instance of new 'pager' class
+$myPager = new Pager(10,'',$prev,$next,'');
+$sql = $myPager->loadSQL($sql);  #load SQL, add offset
+
+# connection comes first in mysqli (improved) function
 $result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
 if(mysqli_num_rows($result) > 0)
-{#there are records - present data
+{#records exist - process
+	if($myPager->showTotal()==1){$itemz = "survey";}else{$itemz = "surveys";}  //deal with plural
+    echo '<div align="center">We have ' . $myPager->showTotal() . ' ' . $itemz . '!</div>';
+    //here is the top of the table
+    echo '
+    <table class="table table-striped table-hover table-responsive">
+  <thead>
+    <tr>
+      <th>Title</th>
+      <th>Admin Name</th>
+      <th>Date Created</th>
+    </tr>
+  </thead>
+  <tbody>
+    ';
+    
+    
 	while($row = mysqli_fetch_assoc($result))
-	{# pull data from associative array
-	   echo '<p>';
-	   echo '<a href="survey_view.php?id=' . $row['SurveyID'] . '">' . $row['Title'] . '</a><br />';
-	   echo 'Description: <b>' . $row['Description'] . '</b><br />';
-	   echo '</p>';
+	{# process each row
+       echo '
+        <tr>
+          <td><a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a></td>
+          <td>' . dbOut($row['AdminName']) . '</td>
+          <td>' . dbOut($row['DateAdded']) . '</td>
+        </tr>
+       ';
+        
+        /*echo '<a href="' . VIRTUAL_PATH . 'surveys/survey_view.php?id=' . (int)$row['SurveyID'] . '">' . dbOut($row['Title']) . '</a>';*/
 	}
+    
+    //here's the bottom of the table
+    echo '
+     </tbody>
+</table> 
+    ';
+	echo $myPager->showNAV(); # show paging nav, only if enough records	 
 }else{#no records
-	echo '<div align="center">Sorry, there are no records that match this query</div>';
+    echo "<div align=center>What! No muffins?  There must be a mistake!!</div>";	
 }
 @mysqli_free_result($result);
-get_footer(); #defaults to footer_inc.php
+
+get_footer(); #defaults to theme footer or footer_inc.php
 ?>
